466cf9bee739f81a9ea30843c05823bfdb48d208
kevinjane71 <malik.vk07@gmail.com>
2025-06-02 17:52:16 +0530

    Fix header import issue - use correct header from ui directory

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Toaster } from 'react-hot-toast'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import Header from './ui/header'
+import Footer from './ui/footer'
 import LayoutWrapper from './components/LayoutWrapper'
 import AuthGuard from './components/AuthGuard'
 
